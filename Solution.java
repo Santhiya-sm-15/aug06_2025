82eaad3855b64072a20f60f57ceb7b5ba678b276
Santhiya <169180434+Santhiya-sm-15@users.noreply.github.com>
2025-08-06 13:47:45 +0530

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int romanToDecimal(String s) {
+        Map<Character,Integer> m=new HashMap<>();
+        m.put('I',1);
+        m.put('V',5);
+        m.put('X',10);
+        m.put('L',50);
+        m.put('C',100);
+        m.put('D',500);
+        m.put('M',1000);
+        int num=m.get(s.charAt(s.length()-1));
+        for(int i=s.length()-2;i>=0;i--)
+        {
+            if(m.get(s.charAt(i+1))>m.get(s.charAt(i)))
+                num-=m.get(s.charAt(i));
+            else
+                num+=m.get(s.charAt(i));
+        }
+        return num;
+    }
+}
